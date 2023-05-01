b9e55a4bb5d76b7403ad69c593679daec9d6de96
EC2 Default User <ec2-user@ip-172-31-31-53.us-west-2.compute.internal>
2023-05-01 20:45:02 +0000

    xyz

--- s3.tf
+++ s3.tf
@@ -1,4 +1,27 @@
 resource "aws_s3_bucket" "example" {
   bucket = "bucket-created-through-iac"
 }
+resource "aws_s3_bucket" "example2" {
+  bucket = " destination-bucket-created-through-iac"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 
+  lifecycle_rule {
+    id      = "archive"
+    enabled = true
+    prefix  = "archive/"
+    transition {
+      days          = 730
+      storage_class = "GLACIER"
+    }
+    expiration {
+      days = 1825
+    }
+  }
+
+}
