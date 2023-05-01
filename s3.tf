eb614067a112c1445af47216834f25e61aa36ac2
EC2 Default User <ec2-user@ip-172-31-31-53.us-west-2.compute.internal>
2023-05-01 20:38:59 +0000

    test3

--- s3.tf
+++ s3.tf
@@ -2,9 +2,3 @@ resource "aws_s3_bucket" "example" {
   bucket = "bucket-created-through-iac"
 }
 
-resource "aws_s3_bucket_object" "example" {
-  bucket = aws_s3_bucket.example.id
-  key    = "example-job-script.py"
-  source = "example-job-script.py"
-
-}
